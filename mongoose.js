930faed8bf885d58239f27808f908825b51c4f6c
Rafael Costa <rafalevita@gmail.com>
2015-11-07 15:40:48 +0000

    deleting node_modules folder

--- mongoose.js
+++ mongoose.js
@@ -1,5 +1,13 @@
+/*eslint-env node */
 var mongoose = require('mongoose');
-var appEnv = require('cfenv');
+var cfenv = require('cfenv');
+
+var appEnv = cfenv.getAppEnv();
+var envVars = appEnv.getEnvVars();
+
+console.log(appEnv);
+console.log("Variables: ");
+console.log(envVars);
 
 var url = appEnv.url || "localhost";
 mongoose.connect('mongodb://' + url + '/edmsdb' );
